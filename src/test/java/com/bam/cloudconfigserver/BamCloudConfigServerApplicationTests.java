079059741e1b3596c0dad5f239c6efcd93b455e7
Shakti Singh <sk.shrinet@gmail.com>
2023-09-14 22:53:28 +0530

    init

---
+++ src/test/java/com/bam/cloudconfigserver/BamCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bam.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
